18dc36a63939ce212511027c914e9ca0e799bfd6
Jeroen van der Poll <110520921+jeroen-vanderpoll@users.noreply.github.com>
2025-09-05 11:11:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';   // <-- make sure this line exists
+import './index.css';           // <-- THIS LINE IS REQUIRED
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
