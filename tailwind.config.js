95f0bc55936b68ac4efa720e6fc509b1f2b1b9fc
Jeroen van der Poll <110520921+jeroen-vanderpoll@users.noreply.github.com>
2025-09-05 11:42:03 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {
-      colors: { slate: { 50: "#f8fafc" } },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
