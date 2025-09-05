edb7c411b0b0134f3822479866ab97f92d81eae5
Jeroen van der Poll <110520921+jeroen-vanderpoll@users.noreply.github.com>
2025-09-05 11:35:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';           // <-- THIS LINE IS REQUIRED
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // <-- important: pulls Tailwind into the bundle
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
